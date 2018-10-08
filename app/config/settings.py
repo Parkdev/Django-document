2d4272a2205dffbf0847746f9f65be04695ee1aa
Jeeho Park <devpark29@gmail.com>
2018-10-08 15:31:19 +0900

    django_extensions추가-오류수정

--- app/config/settings.py
+++ app/config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_extensions]
+    'django_extensions',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
