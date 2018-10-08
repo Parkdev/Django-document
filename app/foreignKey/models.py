53d2fbf23588d14315f876dd48aa41421da7dcea
Jeeho Park <devpark29@gmail.com>
2018-10-08 16:20:30 +0900

    MTO recursivly 예제 구현

--- app/foreignKey/models.py
+++ app/foreignKey/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class Car(models.Model):
+    manufacturer = models.ForeignKey(
+        'Manufacturer',
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50)
@@ -7,12 +17,17 @@ class Manufacturer(models.Model):
     def __str__(self):
         return self.name
 
-class Car(models.Model):
-    manufacturer = models.ForeignKey(
-        Manufacturer,
-        on_delete=models.CASCADE,
+
+
+class FCUser(models.Model):
+    name = models.CharField(max_length=30)
+    instructor = models.ForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='students',
     )
-    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
